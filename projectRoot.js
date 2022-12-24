6d019f96d89d1463e4dd7c05f9bbe508be6c92ab
ahmadshikoh-khan-au49 <ahmadshikohk@gmail.com>
2022-12-24 18:22:22 -0500

    backend project

--- projectRoot.js
+++ projectRoot.js
@@ -13,18 +13,14 @@ app.use(express.json())
 app.use(express.static("./public"))
 app.use(express.urlencoded())
 
-// // app.get("/",(req,res)=>{
-//   res.sendFile("C:\Users\hp\Desktop\projectRepo\backend-project-ahmadshikoh-khan-au49\public\coff.html")
-// // })
 app.get('/booking', getBooking)
 app.get('/booking/:bookingID', getBookingByID)
 app.post('/booking', postBooking)
 app.put('/booking/:bookingID', updateBookingById)
 app.delete('/booking/:bookingID', deleteBookingByID)
 
-//global obj to access all env. variables
-// console.log(process.env)
 
+const PORT = process.env.PORT || 8000
 app.listen(8000, () => {
   console.log("Server Started Successfully")
 })
